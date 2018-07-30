d32c3d801fa3e95922b96ba65da40c0a1f181dfc
Dawid Czartoszewski <dawid.czartoszewski@gmail.com>
2018-07-30 15:55:37 +0200

    fixed application

---
+++ src/main/java/pl/akademiakodu/bmiCalc/controller/BmiController.java
@@ -0,0 +1,24 @@
+package pl.akademiakodu.bmiCalc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.akademiakodu.bmiCalc.model.Value;
+
+
+@Controller
+public class BmiController {
+
+@GetMapping("/")
+public String addValue() {return "/add"; }
+
+@GetMapping("/add")
+    public String showValue (@ModelAttribute Value value ,  ModelMap modelMap ){
+    modelMap.put("index",value.getInd());
+return "/show";}
+
+
+
+
+}
--- src/main/java/pl/akademiakodu/bmiCalc/model/Value.java
+++ src/main/java/pl/akademiakodu/bmiCalc/model/Value.java
@@ -4,14 +4,23 @@ public class Value {
     int weight;
     float height;
 
+
     public int getWeight() {return weight; }
-
     public void setWeight(int weight) { this.weight = weight; }
-
     public float getHeight() { return height; }
-
     public void setHeight(float height) { this.height = height; }
+   public float getInd(){
+        float ind;
+        ind=weight/(height*height);
+        return ind;
+   }
+
+
+
+
 Value(){};
+
+
 
     @Override
     public String toString() {
@@ -20,4 +29,5 @@ Value(){};
                 ", height=" + height +
                 '}';
     }
+
 }
